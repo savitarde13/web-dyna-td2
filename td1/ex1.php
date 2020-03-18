aa1e04a7ce5d25bed9fece0d773433749a38641d
savitarde13 <62104673+savitarde13@users.noreply.github.com>
2020-03-18 11:50:58 +0100

    Ajout du fichier de ex1

---
+++ td1/ex1.php
@@ -0,0 +1,8 @@
+<?php
+
+
+<a href="/web-dyna-td2/td1/ex1.php?message=Hello">Afficher Hello</a>
+<a href="/web-dyna-td2/td1/ex1.php?message=Hola">Afficher Hola</a>
+<a href="/web-dyna-td2/td1/ex1.php?message=salut">Afficher salut</a>
+
+ ?>
