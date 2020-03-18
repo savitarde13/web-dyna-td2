000358e0d85d4c1bb036f3cccf913c70c72c2d3f
savitarde13 <62104673+savitarde13@users.noreply.github.com>
2020-03-18 11:58:15 +0100

    Ajout du fichier de ex1 + fin de séance

--- td1/ex1.php
+++ td1/ex1.php
@@ -4,5 +4,4 @@
 <a href="/web-dyna-td2/td1/ex1.php?message=Hello">Afficher Hello</a>
 <a href="/web-dyna-td2/td1/ex1.php?message=Hola">Afficher Hola</a>
 <a href="/web-dyna-td2/td1/ex1.php?message=salut">Afficher salut</a>
-
- ?>
+?>
